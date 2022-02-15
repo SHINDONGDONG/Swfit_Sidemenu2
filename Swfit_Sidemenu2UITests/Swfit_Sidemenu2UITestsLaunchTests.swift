06b8e447f6d5ec0fa23483de199e8dfd02be976c
mincheol <homerjay@naver.com>
2022-02-15 09:05:35 +0900

    Initial Commit

---
+++ Swfit_Sidemenu2UITests/Swfit_Sidemenu2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swfit_Sidemenu2UITestsLaunchTests.swift
+//  Swfit_Sidemenu2UITests
+//
+//  Created by 申民鐡 on 2022/02/15.
+//
+
+import XCTest
+
+class Swfit_Sidemenu2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
